8c63c54686e1840880d6c1416a76f3f26e757cff
Chrishegyesi <bradshaw888@icloud.com>
2025-06-13 21:13:41 -0400

    getting models

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,23 @@ dotenv.config({ path: '.env' })
 connectDB();
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+// middleware for Json parsing
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    console,log('we are connected at the root endpoint');
+  res.send('API is running...');
+});
+
+app.post('/api/signup', (req,res) => {
+     
+}) 
+
+
+
+
+
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
